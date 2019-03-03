d91ab38f75787fe113be8d814555b027a4a06845
root <thiagomayllart@hotmail.com>
2019-03-03 17:31:08 -0300

    Heuristics finished for Template Injection

--- Raboof/Thread_types.py
+++ Raboof/Thread_types.py
@@ -85,54 +85,6 @@ class Threadpp(Thread):
             #probably not exploitable
             pass
 
-class Threadsi(Thread):
-    def __init__(self, path, headers, data_pay, data_counter_pay, original_request, param_exploited, reqtype, pay_path, counter_pay_path):
-        Thread.__init__(self)
-        if reqtype == 'POST':
-            self.pay_response = HTTPRequester.post_call(path, data_pay, headers)
-            self.counter_pay_response = HTTPRequester.post_call(path, data_counter_pay, headers)
-        else:
-            self.pay_response = HTTPRequester.get_call(pay_path, headers)
-            self.counter_pay_response = HTTPRequester.get_call(counter_pay_path, headers)
-        self.path = path
-        self.headers = headers
-        self.original_request = original_request
-        self.param_exploited = param_exploited
-        self.data_pay = data_pay
-        self.data_counter_pay = data_counter_pay
-        self.reqtype = reqtype
-
-    def run(self):
-        response = ''
-        if self.pay_response.getcode() != self.counter_pay_response.getcode():
-            print 'SOAP INJECTION: '
-            print 'HTTP REPLY CODE DIVERGED: '
-            self.message_function()
-
-        else:
-            if self.pay_response.headers.get('content-length') != self.counter_pay_response.headers.get('content-length'):
-                print 'SOAP INJECTION: '
-                print 'HTTP CONTENT-LENGTH CODE DIVERGED: '
-                self.message_function()
-
-            else:
-                #probably not exploitable
-                pass
-
-    def message_function(self):
-        print 'HTTP REPLY CODE : ' + self.pay_response.getcode + ' and ' + self.counter_pay_response.getcode()
-        print 'HTTP CONTENT-LENGTH ' + self.pay_response.headers.get('content-length') + ' and ' + \
-              self.counter_pay_response.headers.get('content-length')
-        print 'LOCATION: ' + self.param_exploited
-        print 'REQUEST TYPE: ' + self.reqtype
-        print 'PRINTING REQUEST: '
-        print 'PATH :' + self.path
-        print 'FIRST PAYLOAD DATA: ' + self.data_pay
-        print 'SECOND PAYLOAD DATA: ' + self.data_counter_pay
-        for x in self.headers:
-            print x
-            ': ' + self.headers.get(x)
-        print "----------------------------------------------------------------------------------------------\n"
 
 class Threadsi(Thread):
     def __init__(self, path, headers, data_pay, data_counter_pay, original_request, param_exploited, reqtype, pay_path, counter_pay_path):
